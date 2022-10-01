66bbe48eec3aa1f86d23471349347b582fe317af
student.lozin <lozindenis6@gmail.com>
2022-10-01 14:06:04 +0300

    Create work "№3"

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-if start < 7:
-    start += (7 % start)
-elif start > 7:
-    start = (start - (start % 7))+7
-number = start
-while number <= end:
+num1 = int(input('Введіть початкове число - '))
+num2 = int(input('Введіть кінечне число - '))
+
+
+
+if num1 < 3:
+    num1 += (3 % num1)
+elif num1 > 3:
+    num1 = (num1 - (num1 % 3))+3
+number = num1
+while number <= num2:
     print(f"{number}")
-    number += 7
+    number += 3
+if num1<5:
+    num1 +=(5%num1)
+elif num1>5:
+    num1 = (num1 - (num1%5))+5
+number4 = num1
+
+while number4 <= num2:
+    print(f'{number4}')
+    number4 +=7
